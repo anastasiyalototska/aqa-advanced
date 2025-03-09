a78209b32a5eac8e2f3c8bbc6e8b86dac6f905aa
Anastasiya Lototska <anastasiya.lototska@gmail.com>
2025-03-09 23:20:30 +0100

    fix mistake

--- js-basics-cond-loop/Task3.js
+++ js-basics-cond-loop/Task3.js
@@ -2,10 +2,3 @@ const number = 5
 for (let i = 1; i <= 10; i++) {
     console.log(`${number} * ${i} = ${number * i}`)
   }
-
-const number2 = 5;
-let i = 1
-while (i <=10) {
-console.log(`${number2} * ${i} = ${number2 * i}`) 
-i++;
-}
