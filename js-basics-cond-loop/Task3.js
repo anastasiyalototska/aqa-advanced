1f03ba847d609b711ff30cd085e46dcf88e680f1
Anastasiya Lototska <anastasiya.lototska@gmail.com>
2025-03-09 23:23:26 +0100

    homework 5 revision, added while loop

--- js-basics-cond-loop/Task3.js
+++ js-basics-cond-loop/Task3.js
@@ -2,3 +2,10 @@ const number = 5
 for (let i = 1; i <= 10; i++) {
     console.log(`${number} * ${i} = ${number * i}`)
   }
+
+const number2 = 5;
+let i = 1
+while (i <=10) {
+console.log(`${number2} * ${i} = ${number2 * i}`) 
+i++;
+}
